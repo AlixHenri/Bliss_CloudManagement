1d2ee6ec22078464daf1f5c0f5625575e9756151
AlyxHenri <matheushenrif@gmail.com>
2023-01-28 14:22:22 -0300

    Navbar text EN to PT-BR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(true)
   const handleClick = () => setNav(!nav)
   return (
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
@@ -10,30 +10,30 @@ const Navbar = () => {
           <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BLISS.</h1>
           <ul className='hidden md:flex'>
             <li>Home</li>
-            <li>About</li>
-            <li>Support</li>
-            <li>Platforms</li>
-            <li>Pricing</li>
+            <li>Sobre</li>
+            <li>Suporte</li>
+            <li>Plataformas</li>
+            <li>Orçamento</li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
-          <button className='border-none bg-transparent text-black mr-4'>Sign In</button>
-          <button className='px-8 py-3'>Sign Up</button>
+          <button className='border-none bg-transparent text-black mr-4'>Entrar</button>
+          <button className='px-8 py-3'>Cadastrar</button>
         </div>
         <div className="md:hidden cursor-pointer" onClick={handleClick}>
-          {!nav ?  <div className='text-3xl w-10 mt-5'>&#9776;</div> : <div className='text-3xl w-10 mt-5'>&#128473;</div>}  
+          {!nav ? <div className='text-3xl w-10 mt-5'>&#128473;</div> : <div className='text-3xl w-10 mt-5'>&#9776;</div> }  
         </div>
       </div>
 
       <ul className={nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
         <li className='border-b-2 border-zinc-300 w-full'>Home</li>
-        <li className='border-b-2 border-zinc-300 w-full'>About</li>
-        <li className='border-b-2 border-zinc-300 w-full'>Support</li>
-        <li className='border-b-2 border-zinc-300 w-full'>Platforms</li>
-        <li className='border-b-2 border-zinc-300 w-full'>Pricing</li>
+        <li className='border-b-2 border-zinc-300 w-full'>Sobre</li>
+        <li className='border-b-2 border-zinc-300 w-full'>Suporte</li>
+        <li className='border-b-2 border-zinc-300 w-full'>Plataformas</li>
+        <li className='border-b-2 border-zinc-300 w-full'>Orçamento</li>
         <div className='flex flex-col my-4'>
-          <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
-          <button className='px-8 py-3'>Sign Up</button>
+          <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Entrar</button>
+          <button className='px-8 py-3'>Cadastrar</button>
         </div>
       </ul>
     </div>
